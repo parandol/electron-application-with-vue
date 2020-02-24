3a627449fbcc9ceedee76f15f99105da6893f383
Kim Young Gi <parandol76@gmail.com>
2020-02-24 15:56:18 +0900

    Chapter 01 Create Electron Application

---
+++ kr.ejsoft.socket.router.chap01/src/main/index.js
@@ -0,0 +1,44 @@
+import { app } from 'electron'
+import MainWindow from './window/main-window'
+
+let mainWindow = null;
+function init() {
+  mainWindow = MainWindow.create();
+  mainWindow.on('closed', () => {
+    mainWindow = null
+  })
+}
+
+app.on('ready', init)
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', () => {
+  if (mainWindow === null) {
+    init()
+  }
+})
+
+/**
+ * Auto Updater
+ *
+ * Uncomment the following code below and install `electron-updater` to
+ * support auto updating. Code Signing with a valid certificate is required.
+ * https://simulatedgreg.gitbooks.io/electron-vue/content/en/using-electron-builder.html#auto-updating
+ */
+
+/*
+import { autoUpdater } from 'electron-updater'
+
+autoUpdater.on('update-downloaded', () => {
+  autoUpdater.quitAndInstall()
+})
+
+app.on('ready', () => {
+  if (process.env.NODE_ENV === 'production') autoUpdater.checkForUpdates()
+})
+ */
