98b3707fd1bc98172a1ee0805c8037d61f6fd14b
Kim Young Gi <parandol76@gmail.com>
2020-03-04 18:03:18 +0900

    Dark Layout & Frameless Window

---
+++ kr.ejsoft.socket.router.chap09/src/renderer/main.js
@@ -0,0 +1,42 @@
+import Vue from 'vue'
+import axios from 'axios'
+
+import App from './App'
+import router from './router'
+import store from './store'
+
+// Bootstrap Vue
+import { BootstrapVue, BootstrapVueIcons } from 'bootstrap-vue'
+// import 'bootstrap/dist/css/bootstrap.css'
+// import 'bootstrap-vue/dist/bootstrap-vue.css'
+Vue.use(BootstrapVue)
+Vue.use(BootstrapVueIcons)
+import '@babel/polyfill'
+
+
+// Font Awesome
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { far } from '@fortawesome/free-regular-svg-icons'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+library.add(far)
+library.add(fas)
+library.add(fab)
+Vue.component('font-awesome-icon', FontAwesomeIcon)
+
+
+// import { DialogWindow } from '@/components/DialogWindow'
+// Vue.component('frameless-window', DialogWindow)
+
+if (!process.env.IS_WEB) Vue.use(require('vue-electron'))
+Vue.http = Vue.prototype.$http = axios
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+    components: { App },
+    router,
+    store,
+    template: '<App/>'
+}).$mount('#app')
---
+++ kr.ejsoft.socket.router.chap09/src/renderer/views/FontAwesomeIcons.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="flex flex-row flex-wrap">
+    <h4 class="mx-3 my-2">{{ icons.title }}</h4>
+    <template v-for="(icon, key) in icons.icons">
+      <div class="col-4 col-md-3 col-lg-2 col-xl-1 my-1 d-inline-block">
+
+        <template v-if="icons.type === 'bootstrap'">
+          <b-card no-body>
+            <b-card-body class="text-center">
+              <b-icon :icon='icon' font-scale='3'></b-icon>
+            </b-card-body>
+            <b-card-footer class="text-center">
+              {{ icon }}
+            </b-card-footer>
+          </b-card>
+        </template>
+
+        <template v-else>
+          <b-card no-body>
+            <b-card-body class="text-center">
+              <font-awesome-icon :icon="[icon.prefix, icon.iconName]" size="3x" />
+            </b-card-body>
+            <b-card-footer class="text-center">
+              {{ icon.prefix }} {{ icon.iconName }}
+            </b-card-footer>
+          </b-card>
+        </template>
+        
+      </div>
+    </template>
+  </div>
+</template>
+
+<script>
+import Vue from 'vue';
+
+export default {
+    name: "fontawesome-page",
+    components: {
+    },
+    props: {
+      icons: {
+        type: [Array, Object],
+        required:true
+      }
+    },
+    data() {
+      return {
+
+      }
+    },
+    mounted() {
+    },
+    methods : {
+    }
+};
+</script>
